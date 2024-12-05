d81fbc33db0afa04a13e9230ee023b60c521c021
Sam Deane <sam@elegantchaos.com>
2024-12-05 11:14:26 +0000

    Callback needs to be escaping

--- Sources/EmbeddedXCTest/TestHost.swift
+++ Sources/EmbeddedXCTest/TestHost.swift
@@ -13,5 +13,5 @@ public protocol TestHost {
   /// Perform any necessary setup, then call the supplied
   /// closure to run the tests.
   /// The closure returns the number of failures.
-  func embedTests(_ runner: () -> Int)
+  func embedTests(_ runner: @escaping () -> Int)
 }
